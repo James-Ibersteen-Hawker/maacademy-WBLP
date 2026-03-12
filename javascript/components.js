66a61095b998cbfe01bbef78d054109b09e2ee17
James Hawker <526rserbinenko@frhsd.com>
2026-03-12 13:34:01 +0000

    almost done with nav component

--- javascript/components.js
+++ javascript/components.js
@@ -1,33 +1,48 @@
 const navBar = {
   props: {
-    logo: {
-      type: String,
-      default: `/imgs/no-image.png`,
-    },
+    logo: { type: String, default: `/imgs/no-image.png` },
     links: { type: Array, default: () => [] },
+    results: { type: Array, default: () => [] },
+    map: { type: String, default: "#" },
+    phone: { type: String, default: "XXX-XXX-XXXX" },
+    address: { type: String, default: "Unlisted" },
   },
-  emits: ["query"],
+  emits: ["query", "choose"],
   setup(props, { emit }) {
     const query = Vue.ref("");
-    function input(e) {
-      query.value = e.target.value;
-      emit("query", query.value);
-    }
+    const formSubmit = () => emit("query", query.value);
+    const choose = (result) => emit("choose", result);
+    const makePhone = (number) => {
+      // const splitted = number.trim().split(" ").map(e => {e.replace(\(|\))})
+      return number;
+    };
     const formatLink = (link) => {
       if (typeof link !== "string") return "invalid";
-      const newLink = link.trim().toLowerCase().split(/\s+/).join("-")
-      return `/html/${newLink}.html`
-    }
+      const newLink = link.trim().toLowerCase().split(/\s+/).join("-");
+      return `/html/${newLink}.html`;
+    };
     const getLink = (link) => {
       if (typeof link !== "string") return "invalid";
       return link.trim().toLowerCase() === "index" ? "Home" : link.trim();
-    }
-    return { input, query, props, formatLink, getLink };
+    };
+    const showNav = () => {};
+    const hideNav = () => {};
+    return {
+      formSubmit,
+      choose,
+      query,
+      props,
+      formatLink,
+      getLink,
+      showNav,
+      hideNav,
+      makePhone,
+    };
   },
   template: `
   <div class="nav-bar">
-    <div class="nav-bar-close">-></div>
-    <div class="nav-bar-open">X</div>
+    <div class="nav-bar-close" @click="showNav">-></div>
+    <div class="nav-bar-open" @click="hideNav">X</div>
     <div class="nav-bar-header">
       <img :src="props.logo" alt="Music and Art Academy Logo" class="nav-bar-logo">
     </div>
@@ -39,7 +54,18 @@ const navBar = {
       </div>
       <div class="nav-bar-section nav-bar-search">
         <div class="search-icon">Search Icon</div>
-        <div class="fake-search" id="search-input" data-bs-toggle="modal" data-bs-target="#search-modal">Search...</div>
+        <div class="fake-search" id="sham-input" data-bs-toggle="modal" data-bs-target="#search-modal">Search...</div>
+      </div>
+      <div class="nav-bar-section nav-bar-extras row">
+        <div class="col-7 extras-text">
+          <a :href="'tel:' + makePhone(props.phone)">{{props.phone}}</a>
+          <p>{{props.address}}</p>
+        </div>
+        <div class="col-5 extras-map">
+          <a :href="props.map" target="_blank">
+            <img src="/imgs/no-image.png">
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -52,14 +78,22 @@ const navBar = {
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form>
+          <form @submit.prevent="formSubmit">
             <label for="search-input">Search Icon</label>
-            <input v-model="query" id="search-input" name="search-input">
+            <input v-model="query" id="search-input" name="search-input" placeholder="...">
+            <button type="submit">Search</button>
           </form>
+          <div v-if="props.results.length > 0">
+            results
+            <ul>
+              <li v-for="result in props.results" @click="choose(result)">
+                <a :href="result.url" target="_blank">{{result.match}}</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 "use strict";
 const { createApp, ref, reactive } = Vue;
-const worker = new Worker(`/javascript/worker.js`, {type: "module"});
+const worker = new Worker(`/javascript/worker.js`, { type: "module" });
 const pages = [
   "Index",
   "About Us",
@@ -9,15 +9,28 @@ const pages = [
   "Parties",
   "Radio City",
   "Gallery",
-  "Contact"
+  "Contact",
 ];
+class Hit {
+  constructor(match, url) {
+    this.match = match;
+    this.url = url;
+  }
+}
 const App = createApp({
   setup() {
     const content = reactive({ data: null });
-    getSheetData().then((data) => (content.data = data)).catch(err => alert(err));
-    const query = ref(" ");
-    const handleQuery = (val) => (query.value = val);
-    return { query, handleQuery, content, pages };
+    const results = reactive({ data: [] });
+    getSheetData()
+      .then((data) => (content.data = data))
+      .catch((err) => alert(err));
+    function searchSite(input) {
+      results.data = [new Hit("Higgeldy Piggeldy", "link.html")];
+    }
+    function runSelection(selection) {
+      alert(selection.match)
+    }
+    return { searchSite, content, pages, results, runSelection };
   },
 });
 App.component("nav-bar", navBar);
@@ -27,12 +40,12 @@ App.mount("#vue_app");
 
 function getSheetData() {
   return new Promise((resolve, reject) => {
-      worker.onmessage = (e) => {
-        const { data, err } = e.data;
-        if (err) reject(err);
-        else if (!err && data) resolve(data);
-        else reject(new Error("No Data Returned"));
-      };
-      worker.postMessage({ mode: "load" });
+    worker.onmessage = (e) => {
+      const { data, err } = e.data;
+      if (err) reject(err);
+      else if (!err && data) resolve(data);
+      else reject(new Error("No Data Returned"));
+    };
+    worker.postMessage({ mode: "load" });
   });
 }
