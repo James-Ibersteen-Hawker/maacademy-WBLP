6a4dc2eb6effa8a92ec249bb00042abe657d131d
James Hawker <526rserbinenko@frhsd.com>
2026-03-16 12:20:43 +0000

    shortened the sheet function

--- javascript/sheet.js
+++ javascript/sheet.js
@@ -1,33 +1,32 @@
 "use strict";
+const funcLUT = {
+  "front page": frontpage,
+  "teachers + schedules": teachersSched,
+  "contact info + opportunities": contactOpp,
+  "image galleries": imgGalleries,
+};
 export async function sheet(fetchlink, abortTimeout) {
-    const data = await fetch(fetchlink, { signal: AbortSignal.timeout(abortTimeout) });
-    const json = await data.json();
-    const sheets = json.sheets;
-    const entries = Object.entries(sheets);
-    const payload = {
-        
-    }
-    entries.forEach(([name, data]) => {
-        switch (name.toLowerCase()) {
-            case "front page":
-                payload[name] = frontpage(data)
-                break;
-            case "teachers + schedules":
-                payload[name] = teachersSched(data)
-                break;
-            case "contact info + opportunities":
-                payload[name] = contactOpp(data)
-                break;
-            case "image galleries":
-                payload[name] = imgGalleries(data)
-                break;
-            default:
-                break;
-        }
-    })
-    return json;
+  const data = await fetch(fetchlink, {
+    signal: AbortSignal.timeout(abortTimeout),
+  });
+  const sheets = (await data.json())?.sheets ?? {};
+  return Object.entries(sheets).reduce((obj, [name, sheetData]) => {
+    const func = funcLUT[name.trim().toLowerCase()]
+    if (func) obj[name] = func(sheetData);
+    return obj;
+  }, {});
 }
-function frontpage(data) {}
-function teachersSched(data) {}
-function contactOpp(data) {}
-function imgGalleries(data) {}
+
+
+function frontpage(data) {
+  return "lollipop";
+}
+function teachersSched(data) {
+  return "bacon";
+}
+function contactOpp(data) {
+  return "sandwich";
+}
+function imgGalleries(data) {
+  return "skerdoodlydumptious";
+}
