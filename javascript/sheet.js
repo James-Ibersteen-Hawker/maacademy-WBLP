8d9dc8b1b1f30b5ca2b822b5b4b962b4e1d8746c
James Hawker <526rserbinenko@frhsd.com>
2026-03-18 13:16:11 +0000

    maybe I fixed the file paths??

--- javascript/sheet.js
+++ javascript/sheet.js
@@ -23,7 +23,7 @@ export async function sheet(fetchlink, abortTimeout) {
   const timeout = setTimeout(() => controller.abort(), abortTimeout);
   try {
     // const data = await fetch(fetchlink, { signal });
-    const data = await fetch("/test.json", { signal });
+    const data = await fetch("../test.json", { signal });
     const sheets = (await data.json())?.sheets ?? {};
     return Object.entries(sheets).reduce((obj, [name, sheetData]) => {
       const func = funcLUT[name.trim().toLowerCase()];
