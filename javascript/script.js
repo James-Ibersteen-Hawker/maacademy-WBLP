03dad9df537ac8d5eac7eb43fff59f41724ef163
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2026-03-22 18:53:12 -0400

    built the cache!

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,35 @@
 "use strict";
 const { createApp, ref, reactive } = Vue;
-console.log("JS working")
-const weblink = "https://script.google.com/macros/s/AKfycbzYbswK98IKpxzb4J58kxBMEa1-_HFqBkAAsP1GliMghJXUFuEVA1y9v6WCY3a6uLpe/exec";
+console.log("JS working");
+const weblink =
+  "https://script.google.com/macros/s/AKfycbzYbswK98IKpxzb4J58kxBMEa1-_HFqBkAAsP1GliMghJXUFuEVA1y9v6WCY3a6uLpe/exec";
 const signalTimeout = 10000;
-const workerName = location.pathname.includes("/html/") ? '../javascript/worker.js' : "./javascript/worker.js";
+const keys = {
+  dataKey: "sheetData",
+  searchKey: "searchData",
+};
+const hourInMillis = 60 * 60 * 1000;
+const dataTimeout = 12;
+const workerName = location.pathname.includes("/html/")
+  ? "../javascript/worker.js"
+  : "./javascript/worker.js";
 const worker = new Worker(workerName, { type: "module" });
 const pages = [
   new Page("Home", "index.html", "webicons/navbar-icons/home.png"),
-  new Page("About Us", "about-us.html", "webicons/navbar-icons/information.png"),
+  new Page(
+    "About Us",
+    "about-us.html",
+    "webicons/navbar-icons/information.png",
+  ),
   new Page("Contact", "contact.html", "webicons/navbar-icons/phone-call.png"),
   new Page("Classes", "classes.html", "webicons/navbar-icons/music.png"),
   new Page("Teachers", "teachers.html", "webicons/navbar-icons/teacher.png"),
   new Page("Parties", "parties.html", "webicons/navbar-icons/party.png"),
-  new Page("Radio City", "radio-city.html", "webicons/navbar-icons/radio-city.png"),
+  new Page(
+    "Radio City",
+    "radio-city.html",
+    "webicons/navbar-icons/radio-city.png",
+  ),
   new Page("Gallery", "gallery.html", "webicons/navbar-icons/images.png"),
 ];
 const media = [
@@ -27,35 +44,40 @@ const teachers = [
     ["Piano", "Voice"],
     "Jeffrey Pantelas: Music Teacher.Instruments: Piano,Accordion and Electronic keyboards , Vocal training and Chorus.Band leader ,Adonis Orchestra 1981-1992. Music teacher NYC.Bd.of  Education 1985-2016, Project Arts Coordinator P.S.132 Manhattan (9 years).Teacher of piano,accordion and vocal training/chorus Music and Art Academy 2016-present.Education:Accordion and piano 7 years training, Bachelor’s Degree, Baruch college in...",
   ),
-   new Teacher(
+  new Teacher(
     "Jefferey Pantelas",
     "/MAINLOGO.png",
     ["Piano", "Voice"],
     "About Jeff",
   ),
-   new Teacher(
+  new Teacher(
     "Jefferey Pantelas",
     "/MAINLOGO.png",
     ["Piano", "Voice"],
     "About Jeff",
   ),
 ];
-const testInstrument = new Instrument("Piano", [{
-  TEACHERS: {...teachers[0]},
-  SCHEDULES: {
-    "SUN": "",
-    "MON": "P: 3pm-9pm",
-    "TUES": "5:30pm-7pm",
-    "WED": "10am-3pm",
-  }
-}])
+const testInstrument = new Instrument("Piano", [
+  {
+    TEACHERS: { ...teachers[0] },
+    SCHEDULES: {
+      SUN: "",
+      MON: "P: 3pm-9pm",
+      TUES: "5:30pm-7pm",
+      WED: "10am-3pm",
+    },
+  },
+]);
 const App = createApp({
   setup() {
     const copyright = "Copyright 2026 Music and Art Academy";
     const content = ref({});
     const results = reactive({ data: [] });
-    getSheetData()
-      .then((data) => (content.value = data))
+    loadData()
+      .then((data) => {
+        content.value = data;
+        console.log(data);
+      })
       .catch((err) => alert(err.message));
     function searchSite(input) {
       results.data = [
@@ -87,7 +109,7 @@ const App = createApp({
       copyright,
       testEmit,
       teachers,
-      testInstrument
+      testInstrument,
     };
   },
 });
@@ -115,4 +137,22 @@ function getSheetData() {
     worker.onerror = (err) => reject(err);
     worker.postMessage({ mode: "load", link: weblink, timeout: signalTimeout });
   });
+}
+async function loadData() {
+  const slot = localStorage.getItem(keys.dataKey);
+  if (slot) {
+    try {
+      const [payload, time] = JSON.parse(slot);
+      const diff = Date.now() - new Date(time).getTime();
+      const isFresh = diff < dataTimeout * hourInMillis;
+      if (isFresh) return payload;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const data = await getSheetData();
+  if (!data) throw new Error("No Data Fetched!");
+  const storage = JSON.stringify([data, new Date()]);
+  localStorage.setItem(keys.dataKey, storage);
+  return data;
 }
